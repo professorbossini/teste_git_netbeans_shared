fa0aea3862cf85d16343f40ef911ec7315a5d64e
Professor Rodrigo Bossini <professorbossini@gmail.com>
2021-04-23 11:10:00 -0300

    Primeira versão. Já está falando Oi.

---
+++ src/main/java/com/mycompany/teste_git_netbeans_shared/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.teste_git_netbeans_shared;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Oi");
+    }
+}
